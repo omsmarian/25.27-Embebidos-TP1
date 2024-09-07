060f458dc7329b2ee7bdde3345180c3dfaa5add3
valenv25 <vvieira@itba.edu.ar>
2024-09-07 16:21:07 -0300

    Improved Display

--- source/display.c
+++ source/display.c
@@ -20,40 +20,93 @@
 
 #define DEVELOPMENT_MODE		1
 
+#define PWM_FREQUENCY_HZ		(DISPLAY_DIGITS * DISPLAY_MAX_BRIGHTNESS * DISPLAY_FREQUENCY_HZ) // SysTick minimum frequency is 20kHz!!!
+
 // #define DISPLAY_PERIOD_S	(1 / DISPLAY_REFRESH_HZ)
 
 #define CAP(x, min, max)		(x < (min) ? (min) : (x > (max) ? (max) : x))
+#define FOR(_i, _n)				for (uint8_t i = _i; i < _n; i++)
 
 
 /*******************************************************************************
  * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
  ******************************************************************************/
 
+/**
+ * @brief Refresh the display
+ * @note This function is called by the Periodic Interrupt Service Routine (PISR)
+ */
 static void RefreshDisplay (void);
+
+/**
+ * @brief Convert a character to its corresponding segments
+ * @param character Character to convert
+ * @return Corresponding segments
+ * @note If the character is not valid, CLEAR is returned
+ * @example __Char2Segments__('A'); // Returns 0b00110111
+ */
+uint8_t __Char2Segments__ (char character);
 
 
 /*******************************************************************************
  * STATIC VARIABLES AND CONST VARIABLES WITH FILE LEVEL SCOPE
  ******************************************************************************/
 
-static const uint8_t characters [11] = { 0b00111111,
-										 0b00000110,
-										 0b01011011,
-										 0b01001111,
-										 0b01100110,
-										 0b01101101,
-										 0b01111101,
-										 0b00000111,
-										 0b01111111,
-										 0b01101111,
-										 0b00000000 };
+const uint8_t _characters [] = { 0b00111111,   // 0
+								 0b00000110,   // 1
+								 0b01011011,   // 2
+								 0b01001111,   // 3
+								 0b01100110,   // 4
+								 0b01101101,   // 5
+								 0b01111101,   // 6
+								 0b00000111,   // 7
+								 0b01111111,   // 8
+								 0b01101111,   // 9
+								 0b00110111,   // A
+								 0b01111001,   // B
+								 0b00111001,   // C
+								 0b01011110,   // D
+								 0b01111001,   // E
+								 0b01110001,   // F
+								 0b01111100,   // G
+								 0b00110111,   // H
+								 0b00000110,   // I
+								 0b00011110,   // J
+								 0b01110000,   // K
+								 0b00111000,   // L
+								 0b00010101,   // M
+								 0b00110111,   // N
+								 0b00111111,   // O
+								 0b01110011,   // P
+								 0b01100111,   // Q
+								 0b00110001,   // R
+								 0b01101101,   // S
+								 0b01111000,   // T
+								 0b00111110,   // U
+								 0b00111110,   // V
+								 0b00101010,   // W
+								 0b01110110,   // X
+								 0b01101110,   // Y
+								 0b01011011,   // Z
+								 0b01000000,   // DASH
+								 0b10000000,   // DP
+								 0b00000000 }; // CLEAR
 
-// static const uint8_t select [4] = { 0x00, 0x01, 0x02, 0x03 };
+const uint8_t _segments [] = { PIN_DISPLAY_CSEGA,
+							   PIN_DISPLAY_CSEGB,
+							   PIN_DISPLAY_CSEGC,
+							   PIN_DISPLAY_CSEGD,
+							   PIN_DISPLAY_CSEGE,
+							   PIN_DISPLAY_CSEGF,
+							   PIN_DISPLAY_CSEGG,
+							   PIN_DISPLAY_CSEGDP };
 
-static uint8_t display_buffer[DISPLAY_LENGTH] = { 10 };
-// static uint8_t display_index = 0;
+const uint8_t _digits [] = { PIN_DISPLAY_SEL1,
+						     PIN_DISPLAY_SEL0 };
 
-static uint8_t display_brightness = 100;
+static uint8_t _buffer[DISPLAY_DIGITS] = { DISPLAY_CLEAR };
+// static uint8_t _index = 0;
+static uint8_t _brightness = 50;
 
 
 /*******************************************************************************
@@ -64,45 +117,24 @@ static uint8_t display_brightness = 100;
 
 bool DisplayInit (void)
 {
-	static bool init_flag = false;
+	static bool _init = false;
 
-	if (!init_flag) // Already initialized
+	if (!_init && (_init = true)) // Only initialize once
 	{
-		gpioMode(PIN_DISPLAY_CSEGA,		OUTPUT);
-		gpioMode(PIN_DISPLAY_CSEGB,		OUTPUT);
-		gpioMode(PIN_DISPLAY_CSEGC,		OUTPUT);
-		gpioMode(PIN_DISPLAY_CSEGD,		OUTPUT);
-		gpioMode(PIN_DISPLAY_CSEGE,		OUTPUT);
-		gpioMode(PIN_DISPLAY_CSEGF,		OUTPUT);
-		gpioMode(PIN_DISPLAY_CSEGG,		OUTPUT);
-		gpioMode(PIN_DISPLAY_CSEGDP,    OUTPUT);
-		gpioMode(PIN_DISPLAY_SEL0,		OUTPUT);
-		gpioMode(PIN_DISPLAY_SEL1,		OUTPUT);
+		FOR(0, DISPLAY_SEGMENTS)	gpioMode(_segments[i], OUTPUT);
+		FOR(0, DISPLAY_DIGITS / 2)	gpioMode(_digits[i], OUTPUT);
 
-		pisrRegister(RefreshDisplay,  PISR_FREQUENCY_HZ / (DISPLAY_LENGTH * (MAX_BRIGHTNESS) * DISPLAY_FREQUENCY_HZ)); // SysTick minimum frequency is 5000Hz!!!
-
-		init_flag = true;
+		pisrRegister(RefreshDisplay,  PISR_FREQUENCY_HZ / PWM_FREQUENCY_HZ);
 	}
 
-	return init_flag;
+	return _init;
 }
 
-void DisplayWrite (uint8_t characters[])
-{
-	for (uint8_t i = 0; i < DISPLAY_LENGTH; i++)
-		display_buffer[i] = CAP(characters[i], 0, 10);
-}
-
-void DisplayClear (void)
-{
-	for (uint8_t i = 0; i < DISPLAY_LENGTH; i++)
-		display_buffer[i] = 10;
-}
-
-void DisplaySetBrightness (uint8_t brightness)
-{
-	display_brightness = CAP(brightness, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
-}
+void DisplayWrite			(const char* characters)		{ FOR(0, DISPLAY_DIGITS) _buffer[i] = __Char2Segments__(characters[i]); }
+void DisplaySetDigit		(uint8_t digit, char character)	{ _buffer[digit] = __Char2Segments__(character); }
+void DisplayWriteNumber		(uint16_t number)				{ FOR(0, DISPLAY_DIGITS) _buffer[i] = (number / (pow(10, i))) % 10;}
+void DisplayClear			(void)							{ FOR(0, DISPLAY_DIGITS) _buffer[i] = __Char2Segments__(DISPLAY_CLEAR); }
+void DisplaySetBrightness	(uint8_t brightness)			{ _brightness = CAP(brightness, DISPLAY_MIN_BRIGHTNESS, DISPLAY_MAX_BRIGHTNESS); }
 
 
 /*******************************************************************************
@@ -113,102 +145,43 @@ void DisplaySetBrightness (uint8_t brightness)
 
 static void RefreshDisplay (void)
 {
-	static uint8_t display = 0, count = 0;
-	uint8_t character = characters[display_buffer[display]];
+	static uint8_t _index = 0, _count = 0;
+	uint8_t character = _characters[_buffer[_index]];
 
-	if ((count % MAX_BRIGHTNESS) >  display_brightness)
+	if ((_count % DISPLAY_MAX_BRIGHTNESS) >  _brightness) // PWM
 		character = 0;
 
-//	 gpioWrite(PIN_DISPLAY_CSEGA,  0);
-//	 gpioWrite(PIN_DISPLAY_CSEGB,  0);
-//	 gpioWrite(PIN_DISPLAY_CSEGC,  0);
-//	 gpioWrite(PIN_DISPLAY_CSEGD,  0);
-//	 gpioWrite(PIN_DISPLAY_CSEGE,  0);
-//	 gpioWrite(PIN_DISPLAY_CSEGF,  0);
-//	 gpioWrite(PIN_DISPLAY_CSEGG,  0);
-//	 gpioWrite(PIN_DISPLAY_CSEGDP, 0);
+	FOR(0, DISPLAY_DIGITS / 2)	gpioWrite(_digits[i], _index & (1 << i)); // Select digit (Encoded)
+	FOR(0, DISPLAY_SEGMENTS)	gpioWrite(_segments[i], character & (1 << i)); // Select segments (Write character)
 
-	gpioWrite(PIN_DISPLAY_SEL0,   display   & 0x01);
-	gpioWrite(PIN_DISPLAY_SEL1,   display   & 0x02);
+	if ((_count % DISPLAY_MAX_BRIGHTNESS) == 0) // Next digit (Every 4 cycles * 100 steps)
+		_index = ++_index % DISPLAY_DIGITS;
 
-	gpioWrite(PIN_DISPLAY_CSEGA,  character & 0x01);
-	gpioWrite(PIN_DISPLAY_CSEGB,  character & 0x02);
-	gpioWrite(PIN_DISPLAY_CSEGC,  character & 0x04);
-	gpioWrite(PIN_DISPLAY_CSEGD,  character & 0x08);
-	gpioWrite(PIN_DISPLAY_CSEGE,  character & 0x10);
-	gpioWrite(PIN_DISPLAY_CSEGF,  character & 0x20);
-	gpioWrite(PIN_DISPLAY_CSEGG,  character & 0x40);
-	gpioWrite(PIN_DISPLAY_CSEGDP, character & 0x80);
+	_count = ++_count % DISPLAY_MAX_BRIGHTNESS * 4;
+}
 
-    if ((count % MAX_BRIGHTNESS) == 0)
-    {
-    	display++;
-    	display %= DISPLAY_LENGTH;
-    }
-	count++;
-	count %= MAX_BRIGHTNESS * 4;
+// Helper functions ////////////////////////////////////////////////////////////
+
+uint8_t __Char2Segments__ (char character)
+{
+	uint8_t buffer = 0;
+
+		 if (character >= '0' && character <= '9') 	{ buffer = character - '0'; }
+	else if (character >= 'A' && character <= 'Z')	{ buffer = character - 'A' + 10; }
+	else if (character == '-')						{ buffer = 36; } // DASH
+	else if (character == '.')						{ buffer = 37; } // DP
+	else						  					{ buffer = 38; } // CLEAR
+
+	return buffer;
 }
 
 
-
-// #include "pwm.h"
-
-// void DisplayRefresh(void)
-// {
-// 	static uint8_t display = 0, count = 0;
-// 	uint8_t character = characters[display_buffer[display]];
-
-// 	if (count > display_brightness)
-// 		character = 0;
-
-// 	gpioWrite(PIN_DISPLAY_SEL0, display & 0x01);
-// 	gpioWrite(PIN_DISPLAY_SEL1, display & 0x02);
-
-// 	gpioWrite(PIN_DISPLAY_CSEGA, character & 0x01);
-// 	gpioWrite(PIN_DISPLAY_CSEGB, character & 0x02);
-// 	gpioWrite(PIN_DISPLAY_CSEGC, character & 0x04);
-// 	gpioWrite(PIN_DISPLAY_CSEGD, character & 0x08);
-// 	gpioWrite(PIN_DISPLAY_CSEGE, character & 0x10);
-// 	gpioWrite(PIN_DISPLAY_CSEGF, character & 0x20);
-// 	gpioWrite(PIN_DISPLAY_CSEGG, character & 0x40);
-// 	gpioWrite(PIN_DISPLAY_CSEGDP, character & 0x80);
-
-// 	display++;
-// 	display %= 4;
-// 	count++;
-// }
-
-// bool DisplayInit(void)
-// {
-// 	static bool init_flag = false;
-
-// 	if (!init_flag) // Already initialized
-// 	{
-// 		gpioMode(PIN_DISPLAY_CSEGA, OUTPUT);
-// 		gpioMode(PIN_DISPLAY_CSEGB, OUTPUT);
-// 		gpioMode(PIN_DISPLAY_CSEGC, OUTPUT);
-// 		gpioMode(PIN_DISPLAY_CSEGD, OUTPUT);
-// 		gpioMode(PIN_DISPLAY_CSEGE, OUTPUT);
-// 		gpioMode(PIN_DISPLAY_CSEGF, OUTPUT);
-// 		gpioMode(PIN_DISPLAY_CSEGG, OUTPUT);
-// 		gpioMode(PIN_DISPLAY_CSEGDP, OUTPUT);
-// 		gpioMode(PIN_DISPLAY_SEL0, OUTPUT);
-// 		gpioMode(PIN_DISPLAY_SEL1, OUTPUT);
-
-// 		pwmInit(PWM_TIMER, PWM_CHANNEL, PWM_FREQUENCY_HZ);
-// 		pwmWrite(PWM_TIMER, PWM_CHANNEL, display_brightness);
-
-// 		init_flag = true;
-// 	}
-
-// 	return init_flag;
-// }
-
-// void DisplaySetBrightness(uint8_t brightness)
-// {
-// 	display_brightness = CAP(brightness, 0, 100);
-// 	pwmWrite(PWM_TIMER, PWM_CHANNEL, display_brightness);
-// }
-
-
 /******************************************************************************/
+
+// Notes ///////////////////////////////////////////////////////////////////////
+
+// The display is refreshed every 1/50 of a second (20ms) to avoid flickering
+// Input-output? Char? String? Number?
+// _caracters[], etc. location?
+// Brightness levels?
+// PWM: 1 per 100 steps or 1 per step?
--- source/display.h
+++ source/display.h
@@ -19,12 +19,15 @@
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
 
-#define DISPLAY_LENGTH			4
+#define DISPLAY_DIGITS			4
+#define DISPLAY_SEGMENTS		8
 
 #define DISPLAY_FREQUENCY_HZ	50
 
-#define MIN_BRIGHTNESS			0
-#define MAX_BRIGHTNESS			100
+#define DISPLAY_MIN_BRIGHTNESS	0
+#define DISPLAY_MAX_BRIGHTNESS	100
+
+#define DISPLAY_CLEAR       	' '
 
 
 /*******************************************************************************
@@ -32,30 +35,49 @@
  ******************************************************************************/
 
 /**
- * @brief
- * @param
- * @return
+ * @brief Initialize the display
+ * @return Initialization status (true if successful)
+ * @note This function must be called before any other display function
+ * @example if (DisplayInit()) { // Initialization successful }
  */
 bool DisplayInit (void);
 
 /**
- * @brief
- * @param
- * @return
+ * @brief Write characters to the display
+ * @param characters Characters to write (String)
+ * @param length Number of characters to write
+ * @note If length is greater than DISPLAY_LENGTH, only the first DISPLAY_LENGTH characters will be written
+ * @example DisplayWrite("1234", 4); // Write '1234' to the display
  */
-void DisplayWrite (uint8_t characters[]);
+void DisplayWrite (char * characters);
 
 /**
- * @brief
- * @param
- * @return
+ * @brief Set a specific digit in the display
+ * @param digit Digit to set
+ * @param character Character to write
+ * @note Use CLEAR or ' ' to clear the digit
+ * @example DisplaySetDigit(0, 'A'); // Write 'A' to the first digit
+ */
+void DisplaySetDigit (uint8_t digit, char character);
+
+/**
+ * @brief Write a number to the display
+ * @param number Number to write
+ * @note If the number has more than DISPLAY_LENGTH digits, only the last DISPLAY_LENGTH digits will be written
+ * @example DisplayWriteNumber(1234); // Write '1234' to the display
+ */
+void DisplayWriteNumber (uint16_t number);
+
+/**
+ * @brief Clear the display
+ * @example DisplayClear(); // Clear the display
  */
 void DisplayClear (void);
 
 /**
- * @brief
- * @param
- * @return
+ * @brief Set the brightness of the display
+ * @param brightness Brightness level (0-100)
+ * @example DisplaySetBrightness(50); // Set the brightness to 50%
  */
 void DisplaySetBrightness (uint8_t brightness);
 
